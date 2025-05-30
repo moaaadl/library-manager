a2f45ba734c69a2b7cb8e9e0af064d04f32f66e4
moaadl <moadlobi@gmail.com>
2025-05-30 15:57:20 +0100

    add the connection file to db

---
+++ db_connection.py
@@ -0,0 +1,27 @@
+import mysql.connector
+from mysql.connector import Error
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+class Connection:
+    def __init__(self):
+        pass
+
+    def get_connection(self):
+        try:
+            connection = mysql.connector.connect(
+                host=os.getenv("DB_HOST"),
+                user=os.getenv("DB_USER"),
+                password=os.getenv("DB_PASS"),
+                database=os.getenv("DB_NAME")
+            )
+
+            if connection.is_connected():
+                print("\nSuccessfully connected to the database!")
+                return connection
+
+        except Error as e:
+            print(f"Error connecting to the database: {e}")
+            return None
